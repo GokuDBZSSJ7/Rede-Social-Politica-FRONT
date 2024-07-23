8066e2c7951f245ad061b291473d6e308eb0b4fa
Lucas Baggio <lucas.baggio.silva@gmail.com>
2024-07-23 17:51:45 -0300

    Fix: Erro de travar login

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -8,19 +8,19 @@ import { AuthService } from '../services/auth.service';
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private router: Router, private authService: AuthService) {}
+  constructor(private router: Router, private authService: AuthService) { }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     const token = localStorage.getItem('authToken');
     if (token) {
-      this.router.navigate(['feed']);
+      // this.router.navigate(['feed']);
       return true;
     } else {
       this.router.navigate(['login']);
       return false;
     }
   }
-  
+
 }
