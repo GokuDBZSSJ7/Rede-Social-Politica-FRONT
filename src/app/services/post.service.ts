e06d7a9dab274b69d215843c930df99d34577080
Lucas Baggio <lucas.baggio.silva@gmail.com>
2024-07-25 09:56:12 -0300

    Feat: PostService

---
+++ src/app/services/post.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+
+  private apiUrl = 'http://127.0.0.1:8000/api/';
+
+  constructor(private http: HttpClient) { }
+
+  all(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}post`);
+  }
+}
