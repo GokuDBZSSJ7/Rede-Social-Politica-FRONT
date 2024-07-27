dfd26d74ee88e326fdbce040e8deaa99fbc009e4
Lucas Baggio <lucas.baggio.silva@gmail.com>
2024-07-27 15:50:08 -0300

    Form do Candidato

--- src/app/pages/logged/candidates/candidate-modal/candidate-modal.component.ts
+++ src/app/pages/logged/candidates/candidate-modal/candidate-modal.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -10,6 +10,7 @@ import { CityService } from '../../../../services/city.service';
 import { StateService } from '../../../../services/state.service';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { AuthService } from '../../../../services/auth.service';
+import { CandidatesService } from '../../../../services/candidates.service';
 
 @Component({
   selector: 'app-candidate-modal',
@@ -33,6 +34,10 @@ export class CandidateModalComponent implements OnInit {
   cities: any[] = []
   states: any[] = []
   selectedStateId: any;
+  selectedCityId: any;
+  selectedPartyId: any;
+  selectedOfficeId: any;
+  form!: FormGroup;
 
   user: any;
 
@@ -41,11 +46,30 @@ export class CandidateModalComponent implements OnInit {
     private partyService: PartyService,
     private cityService: CityService,
     private stateService: StateService,
-    private authService: AuthService
+    private authService: AuthService,
+    private fb: FormBuilder,
+    private candidateService: CandidatesService
   ) { }
 
   ngOnInit(): void {
     this.listAll();
+    this.createForm();
+  }
+
+  createForm() {
+    this.form = this.fb.group({
+      name: [null, [Validators.required]],
+      age: [null, [Validators.required]],
+      email: [null, [Validators.required]],
+      education: [null, [Validators.required]],
+      experience: [null, [Validators.required]],
+      gender: [null, [Validators.required]],
+      phone: [null, [Validators.required]],
+      office_id: [null, [Validators.required]],
+      party_id: [null, [Validators.required]],
+      city_id: [this.selectedCityId, [Validators.required]],
+      state_id: [this.selectedStateId, [Validators.required]],
+    });
   }
 
   listAll() {
@@ -74,9 +98,28 @@ export class CandidateModalComponent implements OnInit {
     if (this.selectedStateId) {
       this.cityService.getCitiesByStateId(this.selectedStateId).subscribe(data => {
         this.cities = data;
+        this.form.get('state_id')?.setValue(this.selectedStateId);
       });
     } else {
       this.cities = [];
     }
+  }
+
+  onCityChange(): void {
+    this.form.get('city_id')?.setValue(this.selectedCityId);
+  }
+
+  onOfficeChange(): void {
+    this.form.get('party_id')?.setValue(this.selectedPartyId);
+  }
+
+  onPartyChange(): void {
+    this.form.get('office_id')?.setValue(this.selectedPartyId);
+  }
+
+  save() {
+    this.candidateService.create(this.form.value).subscribe({
+
+    })
   }
 }
